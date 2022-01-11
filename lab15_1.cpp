4a2b41d1c177fde240dcb585851cbb1127e23bf7
Aumboon <unaweblanca@gmail.com>
2022-01-11 22:13:56 +0700

    commit

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	int i,j;
+	T key;
+	for(i = 1; i < N; i++){ 
+        cout << "Pass " << i << ":";
+        key = d[i];
+        j = i - 1; 
 
-}
+		while (j >= 0 && d[j] < key){ 
+            d[j + 1] = d[j]; 
+            j = j - 1; 
+        } 
+        d[j + 1] = key; 
+                 for(int z = 0; z < 10; z++) cout << d[z] << " ";
+                 cout << '\n';
+    } 
+    
+} 
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +32,5 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+
